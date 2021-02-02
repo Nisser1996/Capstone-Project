1d6df62756b42451c1efd937a38747be6b133bde
Hayley <hayley0carter@outlook.com>
2021-02-01 17:34:50 -0800

    Infrastructure for App engine and local database complete with basic code to test connection.

---
+++ CPUniversityPortal/src/main/java/TeacherPage.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "TeacherPage",
+    urlPatterns = {"/teacher"}
+)
+public class TeacherPage extends HttpServlet {
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+@Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("This page will eventually allow teachers to log in.\r\n");
+
+  }
+}
