7eb69f4ab4641c8a8c65ae5cc261cf2f91ce0fa3
Hayley <hayley0carter@outlook.com>
2021-02-08 15:18:03 -0800

    App can now add and remove student courses, displays student schedule when the action is taken.

--- CPUniversityPortal/src/main/java/TeacherPage.java
+++ CPUniversityPortal/src/main/java/TeacherPage.java
@@ -10,11 +10,7 @@ import javax.servlet.http.HttpServletResponse;
     urlPatterns = {"/teacher"}
 )
 public class TeacherPage extends HttpServlet {
-
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	databaseController dbController = new databaseController();
 
 @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
