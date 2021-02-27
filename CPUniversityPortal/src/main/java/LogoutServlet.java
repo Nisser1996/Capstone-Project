a1e5c43170224280b0a279f760ea4ba75e61ab21
Hayley <hayley0carter@outlook.com>
2021-02-27 06:59:46 -0800

    Logout Servlet added

---
+++ CPUniversityPortal/src/main/java/LogoutServlet.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Vector;
+
+/******************************************************************************************
+ * 
+ * @author Hayley Carter
+ *
+ */
+@WebServlet(
+    name = "LogoutServlet",
+    urlPatterns = {"/logout"}
+)
+public class LogoutServlet extends HttpServlet {
+	// DatabaseController dbc = new DatabaseController();
+	
+@Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException, ServletException {
+		RequestDispatcher view = request.getRequestDispatcher("/index.html");
+		
+		view.forward(request, response);
+	}
+
+}
+
